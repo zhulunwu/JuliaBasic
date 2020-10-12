266d1c51d5c4f0531217d94e8bfefacbed0b57a5
zlw <zhulunwu@outlook.com>
2020-10-12 13:47:33 +0800

    mnist by tensorflow

--- tensorflow/mnist.jl
+++ tensorflow/mnist.jl
@@ -11,11 +11,12 @@ end
 
 lbls_train=Int8.(mnist_labels())
 
+layers=tf.keras.layers
 model = tf.keras.models.Sequential([
-  tf.keras.layers.Flatten(input_shape=(28, 28)),
-  tf.keras.layers.Dense(128, activation="relu"),
-  tf.keras.layers.Dropout(0.2),
-  tf.keras.layers.Dense(10, activation="softmax")
+  layers.Flatten(input_shape=(28, 28)),
+  layers.Dense(128, activation="relu"),
+  layers.Dropout(0.2),
+  layers.Dense(10, activation="softmax")
 ])
 
 model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
